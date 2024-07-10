a5d90b7687a321231d13985957793460731e9ff7
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-07-10 15:43:43 -0700

    first commit

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <section className='left_sidebar'>
+        <nav className=' flex flex-col gap-6'>
+            <Link href="/" className=' flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'>
+            <Image src="/icons/logo.svg" alt='logo' width={23} height={27}/>
+            <h1 className=' text-24 font-extrabold text-white max-lg:hidden'>Soot AI</h1>
+            </Link>
+        </nav>
+    </section>
+  )
+}
+
+export default LeftSidebar
