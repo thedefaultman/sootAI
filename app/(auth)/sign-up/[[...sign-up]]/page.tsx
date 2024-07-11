8ba87a0b0a61fb7b3b6c09839f0d0a6f660514c1
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-07-11 13:16:36 -0700

    covex and clerk imp. 1/44/25

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+
+
+const page = () => {
+  return (
+    <div className=' flex-center glassmorphism-auth h-screen w-full'>
+        <SignUp />
+    </div>
+  )
+}
+
+export default page
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://still-buffalo-13.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
