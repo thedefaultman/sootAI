9e1a6741faa3ae65d08b31692338f88c8eb0f544
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-07-17 14:54:48 -0700

    Ai thumbnail generator complete

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -8,9 +8,10 @@ import { Loader } from 'lucide-react'
 import { Input } from './ui/input'
 import Image from 'next/image';
 import { useToast } from './ui/use-toast'
-import { useMutation } from 'convex/react'
+import { useAction, useMutation } from 'convex/react'
 import { useUploadFiles } from '@xixixao/uploadstuff/react'
 import { api } from '@/convex/_generated/api'
+import { v4 as uuidv4 } from 'uuid'
 
 const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt }: GenerateThumbnailProps) => {
   const [isAiThumbnail, setIsAiThumbnail] = useState(false)
@@ -22,6 +23,7 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
   const { startUpload } = useUploadFiles(generateUploadUrl)
 
   const getImageUrl = useMutation(api.podcasts.getUrl)
+  const handleGenerateThumbnail = useAction(api.openai.generateThumbnailAction)
 
   const handleImage = async (blob: Blob, fileName: string) => {
     setIsImageLoading(true)
@@ -45,7 +47,21 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
       toast({ title: 'Error generating thumbnail', variant: 'destructive' })
     }
   }
-  const generateImage = async () => {}
+
+  //generate thumbnail using AI
+  const generateImage = async () => {
+    try {
+      const response = await handleGenerateThumbnail({ prompt: imagePrompt })
+      const blob = new Blob([response], { type: 'image/png' })
+      handleImage(blob, `thumbnail-${uuidv4()}`)
+    } catch (error) {
+      console.log(error)
+      toast({ title: 'Error generating thumbnail', variant: 'destructive'})
+    }
+
+  }
+
+  //upload thumbnail image
   const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
 
