6e4f7645e553f7df3368fcca5c22c1eee80245cc
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-07-17 13:11:19 -0700

    generatethumbnail button

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { cn } from '@/lib/utils'
 
 const GenerateThumbnail = () => {
+  const [isAiThumbnail, setIsAiThumbnail] = useState(false)
+
   return (
-    <div>GenerateThumbnail</div>
+    <>
+      <div className=' generate_thumbnail'>
+        <Button 
+          type='button'
+          variant="plain"
+          onClick={() => setIsAiThumbnail(true)}
+          className={cn('', {
+            'bg-black-6': isAiThumbnail
+          })}
+        >
+          Use AI to generate thumbnail
+        </Button>
+
+        <Button 
+          type='button'
+          variant="plain"
+          onClick={() => setIsAiThumbnail(false)}
+          className={cn('', {
+            'bg-black-6': !isAiThumbnail
+          })}
+        >
+          Upload custom image
+        </Button>
+      </div>
+    </>
   )
 }
 
