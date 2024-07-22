1c7848fbad826d4df4b785eb938fc9d369c573a3
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-07-22 15:44:50 -0700

    implement full version

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -10,16 +10,21 @@ import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { useRouter } from 'next/navigation';
 import LoaderSpiner from './LoaderSpiner';
+import { useAudio } from '@/providers/AudioProvider';
+import { cn } from '@/lib/utils';
 
 const RightSidebar = () => {
   const { user } = useUser()
   const topPodcasters = useQuery(api.users.getTopUserByPodcastCount)
   const router = useRouter()
+  const { audio } = useAudio()
 
   if(!topPodcasters) return <LoaderSpiner />
 
   return (
-    <section className='right_sidebar text-white-1'>
+    <section className={cn('right_sidebar h-[calc(100vh-5px)]', {
+      'h-[calc(100vh-140px)]': audio?.audioUrl
+    })}>
       <SignedIn>
         <Link href={`/profile/${user?.id}`} className='flex gap-3 pb-12'>
           <UserButton />
@@ -58,7 +63,7 @@ const RightSidebar = () => {
                 <h2 className='text-14 font-semibold text-white-1'>{podcaster.name}</h2>
               </figure>
               <div className='flex items-center'>
-                <p className='text-12 font-normal'>{podcaster.totalPodcasts} podcasts</p>
+                <p className='text-12 font-normal text-white-2'>{podcaster.totalPodcasts} podcasts</p>
               </div>
             </div>
           ))}
